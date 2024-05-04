10cf25e1d5cb393820f511390d8bdf222b1fe138
Lucy <maffinlp@yahoo.com>
2024-05-04 15:28:24 +0200

    Added Java Backend

---
+++ Seelenkulinarik/src/main/java/at/Seelenkulinarik/Seelenkulinarik/SeelenkulinarikApplication.java
@@ -0,0 +1,13 @@
+package at.Seelenkulinarik.Seelenkulinarik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeelenkulinarikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeelenkulinarikApplication.class, args);
+	}
+
+}
---
+++ Seelenkulinarik/src/test/java/at/Seelenkulinarik/Seelenkulinarik/SeelenkulinarikApplicationTests.java
@@ -0,0 +1,13 @@
+package at.Seelenkulinarik.Seelenkulinarik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeelenkulinarikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
