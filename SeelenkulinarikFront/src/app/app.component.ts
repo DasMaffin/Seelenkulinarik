7cdeb15977193d517e52cc079eb58faae163f0ef
Lucy <maffinlp@yahoo.com>
2024-05-08 08:59:09 +0200

    Added CardService

--- SeelenkulinarikFront/src/app/app.component.ts
+++ SeelenkulinarikFront/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
 import { Card } from './card';
+import { HttpErrorResponse } from '@angular/common/http';
+import { CardService } from './card.service';
 
 @Component({
     selector: 'app-root',
@@ -12,8 +14,25 @@ import { Card } from './card';
 })
 export class AppComponent {
   divs = Array(3).fill(0).map((x, i) => i);
-  card: Card[] = [];
+  cards: Card[] = [];
   
+  constructor(private cardService: CardService){}
+
+  ngOnInit(): void{
+    this.getCards();
+  }
+
+  public getCards(): void{
+    this.cardService.getCards().subscribe(
+      (response: Card[]) => {
+        this.cards = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
   nextSection() {
     throw new Error('Method not implemented.');
   }
---
+++ SeelenkulinarikFront/src/app/card.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Card } from './card';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CardService {
+  private apiServerUrl = 'http://193.34.77.221:8080';
+
+  constructor(private http: HttpClient) { }
+
+  public getCards(): Observable<Card[]>{
+    return this.http.get<Card[]>(`${this.apiServerUrl}/book/all`);
+  }
+}
